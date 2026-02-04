3c2760ff7e982d19e79364a16a01ed0de407f887
Omar Zakaria <115711199+OmarZakaria10@users.noreply.github.com>
2026-02-04 10:42:52 +0200

    Increase request limit to 10 per minute

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -13,7 +13,7 @@ const limiter = rateLimit({
 const limitWindowMs = 60 * 1000; // 1 minute
 const scraperLimiter = rateLimit({
   windowMs: limitWindowMs, // 1 minute
-  max: 3, // Limit each IP to 3 requests per `window` (here, per 1 minute)
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 1 minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: `Too many scraping requests from this IP, please try again after ${limitWindowMs / 1000} seconds.`,
